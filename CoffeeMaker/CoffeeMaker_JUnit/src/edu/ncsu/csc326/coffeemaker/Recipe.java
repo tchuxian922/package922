e52f80ac6d7f5dd62161de43ac5d8d19cafb4d63
HatsyRei <43567407+HatsyRei@users.noreply.github.com>
2018-10-02 21:25:29 +0800

    Fixed issue with "Add recipe" accepting empty strings.

--- CoffeeMaker/CoffeeMaker_JUnit/src/edu/ncsu/csc326/coffeemaker/Recipe.java
+++ CoffeeMaker/CoffeeMaker_JUnit/src/edu/ncsu/csc326/coffeemaker/Recipe.java
@@ -122,10 +122,11 @@ public class Recipe {
     /**
 	 * @param name   The name to set.
 	 */
-    public void setName(String name) {
-    	if(name != null) {
+    public void setName(String name) throws RecipeException {
+    	if(name != null && !name.isEmpty()) {
     		this.name = name;
     	}
+    	else throw new RecipeException("Please input a valid recipe name.");
 	}
     /**
 	 * @return   Returns the price.
